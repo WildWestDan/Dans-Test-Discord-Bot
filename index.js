2c759d018e60a2641df3e36da7b52db84faef0e9
WildWestDan <wildwestdantv@gmail.com>
2022-03-17 16:42:40 -0400

    New bot

---
+++ index.js
@@ -0,0 +1,27 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./auth/auth.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Commands here
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'beep') {
+		await interaction.reply('Boop!');
+	}
+});
+
+// Login to Discord with your client's token
+client.login(token);
